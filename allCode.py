176ae41455a8043c1b2b7a69037a71dbf4c851bd
oliviaemathews17 <olivia.e.mathews@icloud.com>
2026-04-09 17:27:22 -0500

    Add files via upload

---
+++ allCode.py
@@ -0,0 +1,38 @@
+
+def title():
+    print('''
+          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                                  ꒰ঌ࿔────⊹˖₊˚⊹⋆────⋆˚˖⟡˖˚⋆────⋆⊹˚₊˖⊹────࿔໒꒱
+          //ᐢ⑅ᐢ\\   ♡   ₊˚  
+        ꒰ ˶• ༝ •˶꒱       ♡‧₊˚    ♡      WELCOME TO UNIVERSITY SIMULATOR!     ♡‧₊˚    ♡
+        ./づ~ :¨·.·¨:     ₊˚               ~ loyola university chicago ~
+                                                    ────୨ৎ────
+                                        Welcome to your first month, rambler! 
+          
+                                      This week will be full of new experiences,   
+                                      challenges, and opportunities for growth.     𖹭 ₊˚⊹
+                    
+                                As you navigate through your classes, social life, and 
+                               personal well-being, remember that every choice you make 
+                                         will shape your university experience. 
+          
+             Will you prioritize your studies, or will you focus on making friends and enjoying campus life? 
+                    
+                         The decisions are yours to make, and the consequences will follow. 
+          
+          
+
+                   𖹭 ₊˚⊹   Good luck, and have fun exploring the world of university life!   𖹭 ₊˚⊹
+
+                                   
+
+
+
+                       ๋ㅤ ࣭ ㅤ⭑  ☆ㅤ ๋࣭ㅤ ⭑    by olivia, alona, and riley   ๋ㅤ ࣭ ㅤ⭑  ☆ㅤ ๋࣭ㅤ ⭑
+                                    ꒰ঌ࿔────⊹˖₊˚⊹⋆────⋆˚˖⟡˖˚⋆────⋆⊹˚₊˖⊹────࿔໒꒱
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          
+       '''   )
+
+#------------------------------------------------------------------------------------------------------------------
+
